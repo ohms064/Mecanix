7dbc0a6271a260e5bdbf27a5445ecf8aa2ac03d4
Omar Rodríguez Pérez <omar.rodpe@gmail.com>
2017-08-20 23:41:37 -0500

    Se agregó el botón de skip al video.

---
+++ Assets/Proyecto/Scripts/Enviroment/SimpleInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleInteraction : InteractiveBehaviour {
+    public override void Interact( PlayerInteractor interactor ) {
+        for ( int i = 0; i < events.Length; i++ ) {
+            events[i].StartEvent();
+        }
+    }
+
+    public override void Interact( InteractiveBehaviour interactor ) {
+    }
+
+    public override void Restart() {
+    }
+}
---
+++ Assets/Proyecto/Scripts/GameFlow/ObjectRemove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectType {
+    VR, PC
+}
+public class ObjectRemove : MonoBehaviour {
+    public ObjectType type;
+
+    private void Awake() {
+        switch ( type ) {
+            case ObjectType.VR:
+#if UNITY_STANDALONE
+                Destroy( gameObject );
+#endif
+                break;
+            case ObjectType.PC:
+#if UNITY_ANDROID
+                Destroy(gameobject);
+#endif
+                break;
+            default:
+                break;
+        }
+    }
+}
