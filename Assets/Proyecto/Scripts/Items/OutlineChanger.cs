5b9913f5b6ddcfeb16cdcf03e4e5b7bdaa97d5a6
Omar Rodríguez Pérez <omar.rodpe@gmail.com>
2017-08-21 21:21:58 -0500

    Se arreglaron textos. Se arregló glitch que se voltea la cámara. Se agregó un script para recalcar los items.

---
+++ Assets/Proyecto/Scripts/Items/OutlineChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineChanger : MonoBehaviour {
+    public Material mat;
+    public Color color1, color2;
+    public float time = 5f;
+    private float inverseTime;
+
+    public void Start() {
+        inverseTime = 1 / time;
+        StartCoroutine( ColorChange( color1, color2));
+    }
+
+    IEnumerator ColorChange(Color begin, Color to) {
+        float t = 0f;        
+        Color color;
+        while ( t < time ) {
+            yield return new WaitForEndOfFrame();
+            t += Time.deltaTime;
+            color = Color.Lerp( begin, to, t * inverseTime );
+            mat.SetColor( "_OutlineColor", color );
+        }
+        StartCoroutine( ColorChange( to, begin) );
+    }
+}
